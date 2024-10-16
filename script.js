237bccae2fa8ae6fe802026665580c7202e98ee0
Aemon Cao <bot960528@gmail.com>
2024-10-17 00:39:02 +0800

    feat: :sparkles: 在script.js中添加网格材质和渲染器设置

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 
 scene.add(mesh)
@@ -16,9 +16,13 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-
+camera.position.z = 3
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)

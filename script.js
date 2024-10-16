6988da3334c61b0395a7b039a437ec4424825126
Aemon Cao <bot960528@gmail.com>
2024-10-17 00:20:33 +0800

    feat: :sparkles: 在script.js中添加Three.js场景和相机初始化

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('运行JS')
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+
+scene.add(camera)

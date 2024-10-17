da63040ca284bb1da37a1e1da98e345e69859a30
Aemon <yiming.cao@palmaplus.com>
2024-10-17 18:05:06 +0800

    feat: :sparkles: 在script.js中添加多个立方体和旋转动画

--- script.js
+++ script.js
@@ -4,44 +4,53 @@ const canvas = document.querySelector('canvas.webgl')
 
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = - 0.6
-// mesh.position.z = - 1
-mesh.position.set(0.7, -0.6, 1)
+const group = new THREE.Group()
+scene.add(group)
 
-// Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true
+    })
+)
 
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.y = Math.PI * 0.25
-mesh.rotation.x = Math.PI * 0.25
-// mesh.rotation.z = Math.PI * 0.25
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true
+    })
+)
+
+cube2.position.x = -2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+        wireframe: true
+    })
+)
+
+cube3.position.x = 2
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
 
 const sizes = {
     width: 800,
     height: 600
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
-
-camera.lookAt(mesh.position)
-
-// console.log(mesh.position.distanceTo(camera.position))
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper(1)
-scene.add(axesHelper)
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -50,3 +59,19 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 renderer.render(scene, camera)
+
+let time = Date.now()
+
+const render = () => {
+
+    const now = Date.now()
+    const deltaTime = now - time
+    time = now
+    console.log(deltaTime);
+    group.rotation.y += 0.002 * deltaTime
+    group.rotation.x += 0.002 * deltaTime
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(render)
+}
+
+render()

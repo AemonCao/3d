8388c951ac7078fd2cad8bce6c852a2aeb946185
Aemon <yiming.cao@palmaplus.com>
2024-10-17 16:32:33 +0800

    feat: :sparkles: 在script.js中添加网格位置、缩放和旋转设置

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh)
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = - 1
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.z = Math.PI * 0.25
 
 const sizes = {
     width: 800,
@@ -18,6 +34,14 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas

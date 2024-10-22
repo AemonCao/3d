5dd9bb5e19ad077822e0e643cc2e3d6e3410b617
Aemon <yiming.cao@palmaplus.com>
2024-10-22 17:09:32 +0800

    feat: :sparkles: 添加窗口大小调整事件以更新相机和渲染器

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const sizes = {
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
 const cursor = {
     x: 0,
     y: 0
@@ -73,7 +84,7 @@ scene.add(camera)
 
 const orbitControls = new OrbitControls(camera, canvas)
 orbitControls.enableDamping = true
-scene.add(orbitControls)
+scene.add(orbitControls.object)
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas

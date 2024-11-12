10a6d5edbeae2a45d5435aad68cd3ae92165a07f
Aemon <bot960528@gmail.com>
2024-11-12 10:43:51 +0800

    feat: :sparkles: 更新HTML引用为materials.js

---
+++ src/scripts/materials.js
@@ -0,0 +1,51 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons'
+
+const canvas = document.querySelector('canvas.webgl')
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const scene = new THREE.Scene()
+const group = new THREE.Group()
+
+scene.add(group)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+camera.lookAt(group.position)
+scene.add(camera)
+
+const orbitControls = new OrbitControls(camera, canvas)
+orbitControls.enableDamping = true
+scene.add(orbitControls.object)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+function render() {
+  orbitControls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(render)
+}
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+render()

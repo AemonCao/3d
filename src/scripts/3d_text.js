f4419200ce7ee4a529614a9cf662843bfbad2e2c
Aemon <bot960528@gmail.com>
2024-11-13 18:30:56 +0800

    feat: :sparkles: 添加3D文本、材质和优化相机位置

--- src/scripts/3d_text.js
+++ src/scripts/3d_text.js
@@ -1,10 +1,15 @@
 import GUI from 'lil-gui'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons'
+import { FontLoader, OrbitControls, TextGeometry } from 'three/addons'
 import '../style/index.css'
 
 // 用于生成字体文件
 // https://gero3.github.io/facetype.js/
+
+const textureLoader = new THREE.TextureLoader()
+const fontLoader = new FontLoader()
+
+const matcapTexture = textureLoader.load('/src/assets/matcaps/3.png')
 
 const canvas = document.querySelector('canvas.webgl')
 
@@ -25,24 +30,74 @@ const group = new THREE.Group()
 
 scene.add(group)
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xFFFFFF }),
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xFFFFFF }),
+// )
+
+// group.add(cube)
+
+const textGroup = new THREE.Group()
+fontLoader.load(
+  '/src/assets/fonts/helvetiker_regular.typeface.json',
+  (font) => {
+    const textGeometry = new TextGeometry(
+      'Aemon Cao',
+      {
+        font, // 字体
+        size: 0.5, // 字体大小
+        height: 0.2, // 字体厚度
+        curveSegments: 4, // 曲线分段数
+        bevelEnabled: true, // 是否开启斜角
+        bevelThickness: 0.03, // 斜角厚度
+        bevelSize: 0.02, // 斜角大小
+        bevelOffset: 0, // 斜角偏移
+        bevelSegments: 5, // 斜角分段数
+      },
+    )
+
+    textGeometry.center()
+
+    const material = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture,
+      // wireframe: true,
+    })
+    const text = new THREE.Mesh(
+      textGeometry,
+      material,
+    )
+    scene.add(text)
+    console.time('donut')
+
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+    for (let i = 0; i < 500; i++) {
+      const donut = new THREE.Mesh(donutGeometry, material)
+      donut.position.x = (Math.random() - 0.5) * 10
+      donut.position.y = (Math.random() - 0.5) * 10
+      donut.position.z = (Math.random() - 0.5) * 10
+      donut.rotation.x = Math.random() * Math.PI
+      donut.rotation.y = Math.random() * Math.PI
+      const scale = Math.random()
+      donut.scale.set(scale, scale, scale)
+      scene.add(donut)
+    }
+    console.timeEnd('donut')
+  },
 )
 
-group.add(cube)
+scene.add(textGroup)
 
 const cubeFolder = gui.addFolder('Cube')
 cubeFolder.open()
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper(1)
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper(1)
+// scene.add(axesHelper)
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
 camera.lookAt(group.position)
 scene.add(camera)
 
